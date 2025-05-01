aeec28f2196fd7ea169d491a983a2f35c88ea242
Cesare Tagliaferri <taglia@users.noreply.github.com>
2025-05-01 21:09:59 +0800

    Changed to RAIDZ-1

--- scripts/pve-install.sh
+++ scripts/pve-install.sh
@@ -123,7 +123,7 @@ make_answer_toml() {
 
 [disk-setup]
     filesystem = "zfs"
-    zfs.raid = "raid1"
+    zfs.raid = "raidz-1"
     disk_list = ["/dev/vda", "/dev/vdb", "/dev/vdc", "/dev/vdd"]
 
 EOF
